96341d5ec8959f0c20ebb5622f4f7bfa78dc6c82
Stefanie Plas <steffiplas@gmail.com>
2016-06-27 18:11:50 -0400

    part 4

---
+++ plot4.R
@@ -0,0 +1,34 @@
+rawdata <- "./household_power_consumption.txt"
+data <- read.table(rawdata, header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+Mydata <- data[data$Date %in% c("1/2/2007", "2/2/2007"),] #subset data
+
+#Create a new column combining time and date
+DateTime <- strptime(paste(Mydata$Date, Mydata$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+GAP <- as.numeric(Mydata$Global_active_power)
+GRP <- as.numeric(Mydata$Global_reative_power)
+Volt <- as.numeric(Mydata$Voltage)
+SM1 <- as.numeric(Mydata$Sub_metering_1)
+SM2 <- as.numeric(Mydata$Sub_metering_2)
+SM3 <- as.numeric(Mydata$Sub_metering_3)
+
+png("plot4.png", width=480, height=480)
+
+#sets graphing parameters
+par(mfrow = c(2,2))
+
+#sets graph 1
+plot(DateTime, GAP, type = "l", xlab = "", ylab="Global Active Power (kilowatts)")
+
+#sets graph 2
+plot(DateTime, Volt, type = "l", xlab = "datetime", ylab="Voltage")
+
+#sets graph 3
+plot(DateTime, SM1, type = "l", xlab = "", ylab="Energy sub metering")
+lines(DateTime, SM2, type = "l", col = "red")
+lines(DateTime, SM3, type = "l", col = "blue")
+legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+#sets graph 4
+plot(DateTime, GRP, type = "l", xlab = "datetime", ylab="Global_reative_power")
+
+dev.off()
